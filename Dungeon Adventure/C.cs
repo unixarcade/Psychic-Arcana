1e04bff97d6f51d0ffff2e6a71a575b4455562d3
Luminosity <tarturuses_shadow@yahoo.com>
2018-02-23 00:37:32 -0500

    Working on Intra Locution Film
    Will be back to this project afterwards.

--- Dungeon Adventure/C.cs
+++ Dungeon Adventure/C.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Luminosity - Matthew Kowalski
+// https://luminosity.livejournal.com
 
     public static class C
     {
